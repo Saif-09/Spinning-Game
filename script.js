d883a9f384a7c90b52333a4defb36b707cf64306
Saif-09 <saifmd238@gmail.com>
2023-07-01 20:10:17 +0530

    given the condition to return try again

--- script.js
+++ script.js
@@ -1,83 +1,79 @@
+let spinCount = 0;
 
-function shuffle(arr){
-    var currentIndex = arr.length,
+function shuffle(arr) {
+  var currentIndex = arr.length,
     randomIndex;
 
-    while(0!==currentIndex){
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex--;
-        [arr[currentIndex], arr[randomIndex]]=[arr[currentIndex], arr[currentIndex]];
-    }
-    return arr;
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+    [arr[currentIndex], arr[randomIndex]] = [
+      arr[currentIndex],
+      arr[currentIndex],
+    ];
+  }
+  return arr;
 }
 
-function spin(){
-    wheel.play();
-    const box = document.getElementById("box");
-    const element = document.getElementById("mainbox");
-    let SelectItem = "";
+function spin() {
+  spinCount++; // Increment the spin count
 
+  wheel.play();
+  const box = document.getElementById("box");
+  const element = document.getElementById("mainbox");
+  let SelectItem = "";
 
-    let AC = shuffle([1890,2250,2610]);
-    let Camera = shuffle([1850,2210,2570]);
-    let Zonk = shuffle([1770,2130,2490]);
-    let Bike = shuffle([1810, 2170, 2530]);
-    let HeadSet = shuffle([1750, 2110, 2470]);
-    let Drone = shuffle([1630, 1990, 2350]);
-    let SmartPhone = shuffle([1570, 1930, 2290]);
-    let SmartTv = shuffle([1235, 1536, 2344]);
-    let Car = shuffle([3678,2111,2654]);
-    let ipad = shuffle([1892, 1998, 1947]);
-    
-    let results = shuffle([
-        AC[0],
-        Camera[0],
-        Zonk[0],
-        Bike[0],
-        HeadSet[0],
-        Drone[0],
-        SmartPhone[0],
-        SmartTv[0],
-        Car[0],
-        ipad[0]
-
-    ]);
-
-    if(AC.includes(results[0])) SelectItem="AIR CONDITIONER";
-    if(Camera.includes(results[0])) SelectItem = "SONY CAMERA";
-    if (Zonk.includes(results[0])) SelectItem ="ZOMBIE";
-    if (Bike.includes(results[0])) SelectItem= "ELECTRIC BIKE";
-    if (HeadSet.includes(results[0]))SelectItem ="HEADSET";
-    if (Drone.includes(results[0])) SelectItem ="MINI DRONE";
-    if (SmartPhone.includes(results[0])) SelectItem ="SMART PHONE";
-    if (SmartTv.includes(results[0])) SelectItem ="SMART TV";
-    if (Car.includes(results[0])) SelectItem ="ELECTRIC CAR";
-    if (ipad.includes(results[0])) SelectItem ="I PAD";
-    
-
-
+  if (spinCount === 1) {
+    let tryAgain = shuffle([1770, 2130, 2490]);
+    SelectItem = "Try Again";
     box.style.setProperty("transition", "all ease 5s");
-    box.style.transform = "rotate(" + results[0] + "deg)";
-    element.classList.remove("animate");
-    setTimeout(function(){
-        element.classList.add("animate");
-    },5000);
+    box.style.transform = "rotate(" + tryAgain[0] + "deg)";
+    setTimeout(function () {
+    Swal.fire({
+      title: "Sorry...",
+      text: "You lost. Please try again!",
+      icon: "error",
+      });
+    },5500);
+}
 
-    setTimeout(function(){
+   else {
+    let prizes = [
+      { name: "AIR CONDITIONER", values: shuffle([1890, 2250, 2610]) },
+      { name: "SONY CAMERA", values: shuffle([1850, 2210, 2570]) },
+      { name: "ELECTRIC BIKE", values: shuffle([1810, 2170, 2530]) },
+      { name: "HEADSET", values: shuffle([1750, 2110, 2470]) },
+      { name: "MINI DRONE", values: shuffle([1630, 1990, 2350]) },
+      { name: "SMART PHONE", values: shuffle([1570, 1930, 2290]) },
+      { name: "SMART TV", values: shuffle([1235, 1536, 2344]) },
+      { name: "ELECTRIC CAR", values: shuffle([3678, 2111, 2654]) },
+      { name: "I PAD", values: shuffle([1892, 1998, 1947]) },
+    ];
+    let randomIndex = Math.floor(Math.random() * prizes.length);
+    let prize = prizes[randomIndex];
+    SelectItem = prize.name;
+    box.style.setProperty("transition", "all ease 5s");
+    box.style.transform = "rotate(" + prize.values[0] + "deg)";
+
+    setTimeout(function () {
         Swal.fire({
-            title: 'Congratulations........',
-            html: 'You Won '+ SelectItem + ' | ' + '<a href="#"> Claim Now </a>',
-            imageUrl: './assets/lucky draw logo.png',
-            imageWidth: 400,
-            imageHeight: 300,
-            imageAlt: 'Custom image',
-          })
+          title: "Congratulations........",
+          html: "You Won " + SelectItem + " | " + '<a href="#"> Claim Now </a>',
+          imageUrl: "./assets/lucky draw logo.png",
+          imageWidth: 400,
+          imageHeight: 300,
+          imageAlt: "Custom image",
+        });
+      }, 5500);
 
-    },5500)
+  element.classList.remove("animate");
+  setTimeout(function () {
+    element.classList.add("animate");
+  }, 5000);
 
-    setTimeout(function(){
-        box.style.setProperty("transition", "initial");
-        box.style.transform = "rotate(90deg)";
-    },6000);
-
+  setTimeout(function () {
+    box.style.setProperty("transition", "initial");
+    box.style.transform = "rotate(90deg)";
+  }, 6000);
+}
 }
